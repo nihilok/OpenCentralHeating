f3aa0e3280616c836f3207af3ce8c9148d8559a8
= <=>
2021-11-23 09:22:36 +0000

    access token expire

--- api/auth/constants.py
+++ api/auth/constants.py
@@ -1,11 +1,11 @@
 SUPERUSERS = [1]
 GUEST_IDS = [3]
-TESTING = True  # Uses benign heating system when True
+TESTING =  False # Uses benign heating system when True
 # API will fail if no heating system in place when False
 
-origins = ["https://heating.jarv.app"]
+origins = ["https://example.app"]
 
 origins = origins + ["*"] if TESTING else origins
-SECRET_KEY = "SoMeThInG_-sUp3Rs3kREt!!"
+SECRET_KEY = "asdasfsdkjglksjdflkfnalsmdn/alsdn1SoMeThInG_-sUp3Rs3kREt!!"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 5
