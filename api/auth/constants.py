1ed9fd569865c6de2818f979bace7161fbeb6337
= <=>
2021-11-23 09:24:00 +0000

    access token expire

--- api/auth/constants.py
+++ api/auth/constants.py
@@ -1,11 +1,11 @@
 SUPERUSERS = [1]
 GUEST_IDS = [3]
-TESTING =  False # Uses benign heating system when True
+TESTING = True  # Uses benign heating system when True
 # API will fail if no heating system in place when False
 
 origins = ["https://example.app"]
 
 origins = origins + ["*"] if TESTING else origins
-SECRET_KEY = "asdasfsdkjglksjdflkfnalsmdn/alsdn1SoMeThInG_-sUp3Rs3kREt!!"
+SECRET_KEY = "SoMeThInG_-sUp3Rs3kREt!!"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_DAYS = 5
