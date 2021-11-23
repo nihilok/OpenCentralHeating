abdb422f608399ddb4940258ded7f0fed19e9b99
= <=>
2021-11-23 09:29:00 +0000

    heating constants

--- api/heating/constants.py
+++ api/heating/constants.py
@@ -2,6 +2,6 @@ WEATHER_URL = "https://api.openweathermap.org/data/2.5/onecall?lat=51.6862" \
               "&lon=-1.4129&exclude=minutely,hourly&units=metric" \
               "&appid=<<YOUR-API-KEY>>"
 
-TEMPERATURE_URL = 'https://api.smarthome.mjfullstack.dev'
+TEMPERATURE_URL = 'https://example.sensor.api.com'
 
 GPIO_PIN = 27
