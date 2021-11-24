a3bb6c087c3a56b77de5b69da74f42aec458ac1d
nihilok <mjfullstack@gmail.com>
2021-11-24 23:12:27 +0000

    improved install script

--- install.py
+++ install.py
@@ -42,42 +42,58 @@ subprocess.run(["ln", "run.sh", "/usr/local/bin/open-heating"])
 os.setuid(UID)
 LOCAL_DIR = os.path.abspath(os.path.dirname(__file__))
 os.chdir(LOCAL_DIR)
-print("creating Python virtual environment...")
-subprocess.run(["python3", "-m", "venv", "env"])
-print("Installing requirements")
-subprocess.run(
-    [f"{LOCAL_DIR}/env/bin/pip", "install", "-r", "requirements.txt"],
-)
-with subprocess.Popen(
-    [f"{LOCAL_DIR}/env/bin/python3", "main.py"],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT,
-    universal_newlines=True,
-) as process:
-    print("Creating database...")
-    for line in process.stdout:
-        if "Application startup complete" in line:
-            print("API started successfully")
-            subprocess.run(
-                [
-                    "wget",
-                    "-O",
-                    "/dev/null",
-                    "-o",
-                    "/dev/null",
-                    "http://localhost:8080/docs",
-                ]
-            )
-            process.send_signal(signal.SIGINT)
-        if "Stopping reloader process" in line:
-            print(
-                'Installation  complete!\n\nUse command "open-heating" to run the server.'
-            )
-            process.send_signal(signal.SIGINT)
-    conn = sqlite3.connect("db.sqlite3")
-    c = conn.cursor()
-    if not len(list((c.execute("SELECT * FROM householdmember where id=1")))):
-        print("=====================================================")
-        print("Please create a superuser...", end="\n\n")
-        subprocess.run([f"{LOCAL_DIR}/env/bin/python", "create_superuser.py"])
-    conn.close()
+if not os.path.exists('env'):
+    print("creating Python virtual environment...")
+    subprocess.run(["python3", "-m", "venv", "env"])
+    with subprocess.Popen(
+        [f"{LOCAL_DIR}/env/bin/pip", "install", "-r", "requirements.txt"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    ) as process:
+        print("Installing requirements")
+        for line in process.stdout:
+            pass
+        print("Done")
+else:
+    print('WARNING: Virtual environment already exists')
+if not os.path.exists('db.sqlite3'):
+    with subprocess.Popen(
+        [f"{LOCAL_DIR}/env/bin/python3", "main.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        universal_newlines=True,
+    ) as process:
+        print("Creating database...")
+        for line in process.stdout:
+            if "Application startup complete" in line:
+                print("API started successfully")
+                subprocess.run(
+                    [
+                        "wget",
+                        "-O",
+                        "/dev/null",
+                        "-o",
+                        "/dev/null",
+                        "http://localhost:8080/docs",
+                    ]
+                )
+                process.send_signal(signal.SIGINT)
+            if "Stopping reloader process" in line:
+                print(
+                    'Installation  complete!\n\nUse command "open-heating" to run the server.',
+                    end="\n\n",
+                )
+                process.send_signal(signal.SIGINT)
+else:
+    print('WARNING: db.sqlite3 already exists')
+
+conn = sqlite3.connect("db.sqlite3")
+c = conn.cursor()
+if not len(list((c.execute("SELECT * FROM householdmember where id=1")))):
+    print("=====================================================")
+    print("Please create a superuser...", end="\n\n")
+    subprocess.run([f"{LOCAL_DIR}/env/bin/python", "create_superuser.py"])
+else:
+    print('Superuser already exists')
+conn.close()
