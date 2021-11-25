6249c6a8482988648f0cc15a8d621c3c3a00a3a1
nihilok <mjfullstack@gmail.com>
2021-11-25 09:04:57 +0000

    improve create superuser

--- create_superuser.py
+++ create_superuser.py
@@ -1,9 +1,19 @@
+import json
 import sqlite3
+import configparser
+import os
+from pathlib import Path
 from getpass import getpass
 
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+config = configparser.ConfigParser()
+path = Path(__file__)
+ROOT_DIR = path.parent.absolute()
+config_path = os.path.join(ROOT_DIR, "api/secrets/secrets.ini")
 
 
 def get_password_hash(password):
@@ -24,6 +34,10 @@ def create_superuser(household: int = 1):
     check_for_first_household()
     name = input("name: ")
     password = get_password_hash(getpass("password: "))
+    confirm_password = get_password_hash(getpass("confirm_password: "))
+    if password != confirm_password:
+        print('**PASSWORDS DO NOT MATCH, please try again**')
+        return create_superuser()
     conn = sqlite3.connect("db.sqlite3")
     curs = conn.cursor()
     vals = f'("{name}", "{password}", {household})'
@@ -31,7 +45,17 @@ def create_superuser(household: int = 1):
         f"INSERT INTO householdmember(name, password_hash, household_id) VALUES {vals}"
     )
     conn.commit()
+    superuser_id = curs.lastrowid
     conn.close()
+    superuser_conf = config['SUPERUSERS']
+    if superuser_conf['superuser_list']:
+        superusers = json.loads(superuser_conf['superuser_list'])
+        superusers.append(superuser_id)
+    else:
+        superusers = [superuser_id]
+    superuser_conf['superuser_list'] = json.dumps(superusers)
+    with open(config_path, 'w') as f:
+        config.write(f)
 
 
 if __name__ == "__main__":
