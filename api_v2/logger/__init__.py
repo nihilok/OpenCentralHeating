b4fd902d0a58c3acaf50a7a6f536ba777a459708
nihilok <mjfullstack@gmail.com>
2021-12-05 11:42:06 +0000

    log format

--- api_v2/logger/__init__.py
+++ api_v2/logger/__init__.py
@@ -2,16 +2,22 @@ import logging
 import logging.handlers as handlers
 
 LOG_FILE = "/var/log/heating/heating.log"
+BACKUP_LOG_FILE = "heating.log"
 
 
 def get_logger(name=__name__):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
 
-    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+    formatter = logging.Formatter("%(levelname)s:\t  %(message)s [%(asctime)s] (%(name)s)")
 
     try:
         fh = handlers.RotatingFileHandler(LOG_FILE, maxBytes=2000000, backupCount=2)
+        fh.setLevel(logging.INFO)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+    except PermissionError:
+        fh = handlers.RotatingFileHandler(BACKUP_LOG_FILE, maxBytes=2000000, backupCount=2)
         fh.setLevel(logging.INFO)
         fh.setFormatter(formatter)
         logger.addHandler(fh)
